afc14b1014bfbb1d779c50f0f3ad8102da364c62
Mohsin47 <39695895+Mohsin47@users.noreply.github.com>
2021-10-09 15:12:07 +0500

    Add files via upload

---
+++ Barchart.js
@@ -0,0 +1,60 @@
+﻿$(function ()
+
+{
+    debugger;
+    $.ajax({
+       
+
+        url: "/Home/GetData",
+        type: "json",
+        method: "Get",
+
+        success: function (data) {
+
+
+            var Name = [];
+            var Qts = [];
+
+            for (var i = 0; i < data.length; i++) {
+                Name.push(data[i].name);
+                Qts.push(data[i].count);
+            }
+
+            debugger;
+            var ctx_bar = document.getElementById("myBarChat");
+
+            debugger;
+            var MyBarChart = new Chart(ctx_bar, {
+
+                debugger:true,
+                type: 'bar',
+                data: {
+
+                    labels: Name,
+                    datasets: [{
+
+                        label: 'Earning',
+                        backgroundColor: "rgba(255,99,132,0.2)",
+                        borderColor: "rgba(255,99,132,1)",
+                        borderWidth: 2,
+                        hoverBackgroundColor: "rgba(255,99,131,0.4)",
+                        hoverBorderColor: "rgba(255,99,131,1)",
+                        data: Qts
+
+
+                    }]
+
+                }
+
+
+            });
+
+        }
+
+
+    });
+
+
+});
+
+
---
+++ PieChartController.cs
@@ -0,0 +1,53 @@
+﻿using LineChartDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LineChartDemo.Controllers
+{
+    public class PieChartController : Controller
+    {
+        private readonly ApplicationDataContext _db;
+
+        public PieChartController(ApplicationDataContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult GetData()
+        {
+
+            int male = _db.Peopels.Where(m => m.Gender == "Male").Count();
+            int female = _db.Peopels.Where(m => m.Gender == "Female").Count();
+            Data data = new Data();
+            data.Male = male;
+            data.Female = female;
+
+            return Json(data);
+        }
+
+        public class Data {
+            public int Male { get; set; }
+            public int Female { get; set; }
+
+        }
+
+
+
+
+        public IActionResult BarChartData()
+        {
+            return View();
+        }
+    }
+}
